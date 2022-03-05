9780537de6923ddef4510f77d2409c0b9faf005d
Miten Chauhan <miten@chauhan.nz>
2022-03-06 10:11:32 +1300

    fix game height on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import {
   Box,
   ChakraProvider,
+  extendTheme,
   Flex,
-  theme,
 } from "@chakra-ui/react";
 import { GameBoard } from "./components/game";
 import { Keyboard } from "./components/keyboard";
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      "html, body, #root": {
+        height: "100%",
+      },
+    },
+  },
+});
+
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Flex textAlign="center" fontSize="xl" height="100vh" width="100vw" p={3} flexDirection="column">
+    <Flex textAlign="center" fontSize="xl" height="100%" width="100vw" p={3} flexDirection="column">
       <Box flexGrow={1}>
         <GameBoard />
       </Box>
