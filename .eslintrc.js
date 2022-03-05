325bd9c6f6a01e7335f821c342c40f753a7e3e06
Miten Chauhan <miten@chauhan.nz>
2022-03-05 14:37:38 +1300

    Inital setup

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+  ],
+  rules: {
+    "comma-dangle": [
+      "error",
+      "always-multiline",
+    ],
+    "indent": [
+      "error",
+      2,
+      { "SwitchCase": 1 },
+    ],
+    "linebreak-style": [
+      "error",
+      "unix",
+    ],
+    "quotes": [
+      "error",
+      "double",
+    ],
+    "react/prop-types": "off",
+    "semi": [
+      "error",
+      "always",
+    ],
+  },
+};
